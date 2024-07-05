98fd43fc440a80ceb18750c7eff9b48bb65a9406
Jose I Cabral Farre <jose@appenate.com>
2024-07-05 13:43:38 -0300

    Fixed further issues caused by incorrect import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useMatch } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
@@ -36,12 +36,12 @@ const App = () => {
   }
 
   return (
-      <Routes>
-        <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
-        <Route path="/pokemon/:name" element={
-          <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-        } />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<PokemonList pokemonList={pokemonList} />} />
+      <Route path="/pokemon/:name" element={
+        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
+      } />
+    </Routes>
   )
 }
 
